be6f6332227a3663732a8df1e8350d2054ff1477
Totterynine <totterynine@gmail.com>
2021-02-02 20:34:07 +0300

    Added base bludgeon class (REGENERATE VPC)
    preparation

--- game/shared/vance/weapons/vance_baseweapon_shared.cpp
+++ game/shared/vance/weapons/vance_baseweapon_shared.cpp
@@ -617,3 +617,16 @@ void CVanceMachineGun::ItemPostFrame(void)
 }
 
 #endif
+
+// HACKHACK: need to put entire VanceBludgeonWeapon here later. this is a stub
+#ifndef GAME_DLL
+class C_VanceBludgeonWeapon : public CBaseVanceWeapon
+{
+public:
+	DECLARE_CLASS(C_VanceBludgeonWeapon, CBaseVanceWeapon);
+	DECLARE_CLIENTCLASS();
+};
+
+IMPLEMENT_CLIENTCLASS_DT(C_VanceBludgeonWeapon, DT_VanceBludgeonWeapon, CVanceBludgeonWeapon)
+END_RECV_TABLE()
+#endif
