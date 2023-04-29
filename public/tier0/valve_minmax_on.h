77219158fdb97a74fd916edaac8486184c5db599
Totterynine <totterynine@gmail.com>
2023-04-29 10:51:32 +0500

    Remove minmax macro redefinition

--- public/tier0/valve_minmax_on.h
+++ public/tier0/valve_minmax_on.h
@@ -1,9 +1,9 @@
 //========= Copyright Valve Corporation, All rights reserved. ============//
-#if !defined(POSIX)
-#ifndef min
-	#define MIN(a,b)  (((a) < (b)) ? (a) : (b))
-#endif
-#ifndef max
-	#define MAX(a,b)  (((a) > (b)) ? (a) : (b))
-#endif
-#endif
+//#if !defined(POSIX)
+//#ifndef min
+//	#define MIN(a,b)  (((a) < (b)) ? (a) : (b))
+//#endif
+//#ifndef max
+//	#define MAX(a,b)  (((a) > (b)) ? (a) : (b))
+//#endif
+//#endif
